9c7a4b45e8c64ca3be0d72393a2dfc7e8725bf03
daniilsergx <daniil.serga@gmail.com>
2023-01-06 22:17:30 +0300

    added a lecture and a few exercises for this theme

--- Exercise#3/exercise.js
+++ Exercise#3/exercise.js
@@ -1,1 +1,71 @@
-// OOP
+// OOП
+
+// #1: Создать объект прямоугольник и добавить в него свойства длины двух сторон и функцию вычисления площади
+// затем удалить свойство длины второй стороны (будем считать, что теперь это объект квадрат)
+// и заново посчитать площадь
+let firstSideLength = 4;
+let secondSideLength = 6;
+
+let figure = {
+    firstSideLength,
+    secondSideLength,
+    calculateSquare() {
+        return this.secondSideLength == undefined ? this.firstSideLength**2 : this.firstSideLength * this.secondSideLength;
+    }
+}
+
+console.log(`Площадь прямоугольника: ${figure.calculateSquare()}`);
+
+delete figure.secondSideLength;
+
+console.log(`Площадь квадрата: ${figure.calculateSquare()}\n`);
+
+// #2: Есть некий директор компании, у которого в подчинении находятся сотрудники. Необходимо представить
+// директора как объект, содержащий в себе набор объектов сотрудников и добавить функции 
+// подсчёта количества сотрудников и переименования компании
+let employees = [
+    { name: 'Эрик', age: 28 },
+    { name: 'Сэм', age: 29 },
+    { name: 'Джон', age: 21},
+    { name: 'Карл', age: 34},
+    { name: 'Эдуард', age: 26 }
+]
+
+const employer = {
+    "company name": 'Растишка',
+    "name": 'Станислав',
+    "age": 19,
+    employees,
+    changeCompanyName(companyName) {
+        console.log(`Название компании изменено с ${this["company name"]} на ${companyName}`);
+        this["company name"] = companyName;
+    },
+    countEmployees() {
+        console.log(`Количество сотрудников - ${this.employees.length}:`);
+
+        for (let i = 0; i < this.employees.length; i++) {
+            console.log(`${i + 1}) Имя: ${employees[i].name}, Возраст: ${employees[i].age}`);
+        }
+    },
+    addEmployee(employee) {
+        if (employee.age == undefined || isNaN(employee.age) || employee.name == undefined) {
+            return;
+        }
+
+        console.log(`В компанию добавлен новый сотрудник:\nИмя: ${employee.name}, Возраст: ${employee.age}\n`);
+        this.employees.push(employee);
+    },
+    info() {
+        console.log('ИНФОРМАЦИЯ О ДИРЕКТОРЕ');
+        console.log(`Имя: ${this["name"]}`);
+        console.log(`Возраст: ${this["age"]}`);
+        console.log(`Название компании: ${this["company name"]}`);
+        console.log(`Количество подчинённых: ${this.employees.length}\n`);
+    }
+}
+
+employer.info();
+employer.addEmployee({ name: 'Иван', age: 18 });
+employer.countEmployees();
+employer.changeCompanyName('Клиренс');
+console.log(`Название компании: ${employer["company name"]}`);
--- Exercise#3/lesson.js
+++ Exercise#3/lesson.js
@@ -1,1 +1,130 @@
-// OOP
+// OOП
+
+// Объект
+let user = {
+    name: 'Юрий',
+    age: 21,
+}
+
+let teacher = {
+    user,
+    course: 'JavaScript',
+}
+
+// {name: 'Юрий', age: 21} JavaScript
+console.log(teacher.user, teacher.course);
+
+// Объект также может содержать в себе функции. Эти функции могут как 
+// обращаться к свойствам класса с помощью 'this.', так и наоборот.
+// Также могут возвращать значения и принимать параметры.
+// короткая форма записи функции в объекте: название_функции(): { команды }
+// полная форма записи функции в объекте: название_функции: function() { команды }
+user = {
+    name: 'Виталий',
+    age: 27,
+    isOlderThan(user) {
+        return this.age >= user.age ? true : false;
+    },
+    getBirthDate() {
+        return new Date().getUTCFullYear() - this.age;
+    },
+    info: function() {
+        console.log('ИНФОРМАЦИЯ');
+        console.log(`Имя: ${this.name}`);
+        console.log(`Возраст: ${this.age}`);
+    }
+}
+
+user.info();
+// 1996
+console.log(user.getBirthDate());
+// true
+console.log(user.isOlderThan({ age: 24 }));
+
+// Также, названия свойств объекта могут состоять из нескольки слов. В таком случае
+// названия определяются с помощью синтаксиса массивов: ["название"]: значение
+// либо "название": значение
+user = {
+    ["date of birth"]: '19.06.2007',
+    "full name": 'Иванов Иван Иванович',
+    age: 16
+}
+
+// Обращение к сложным свойствам происходит как при обращении к элементу массива - через [], где
+// в квадратных скобках указывается название свойства
+console.log(`\nДата рождения: ${user['date of birth']}`);
+console.log(`ФИО: ${user['full name']}`);
+console.log(`Возраст: ${user.age}\n`);
+
+// Также в объектах можно динамически задавать названия и значения свойств:
+let obj_1 = getDynamicObject('name', 'Эрик');
+let obj_2 = getDynamicObject('filmTitle', 'Хатико');
+obj_1.print();
+obj_2.print();
+
+function getDynamicObject(propName, propValue) {
+    let obj = {
+        // Здесь динамически свойству задаётся название и значение
+        [propName]: propValue,
+        print() {
+            console.log(`${propName}: ${propValue}`);
+        }
+    }
+
+    return obj;
+}
+
+// Также JavaScript представляет возможность удалять свойства объектов с помощью команды .delete
+user = {
+    name: 'Филипп',
+    age: 28,
+    city: 'Будапешт',
+    info() {
+        console.log(`\nИмя: ${this.name}`);
+        console.log(`Возраст: ${this.age}`);
+        console.log(`Город: ${this.city}`);
+    }
+}
+
+user.info(); // выведется вся информация
+delete user.city; // удаление свойства
+if (user.city == undefined) {
+    console.log('Свойство city было удалено!\n');
+}
+
+// Возможно, возникнет необходимость создать неизменяющийся (константный) класс.
+// Для этого недостаточно будет написать const название_объекта. Таким образом
+// возможность редактировать свойства останется, а инициализировать заново сам объект - нет
+const nonConstantUser = {
+    name: 'Боб'
+}
+
+nonConstantUser.name = 'Елизаветта';
+console.log(nonConstantUser.name); // Елизаветта
+
+// Для того чтобы исключить возможность изменения класса необходимо "заморозить его":
+Object.freeze(nonConstantUser); // - заморозка объекта
+nonConstantUser.name = 'Боб'; // - присваивание нового значения свойству
+console.log(nonConstantUser.name); // Всё равно выведет Елизаветта
+
+console.log('');
+// Также для сокращения количества строк кода, можно сокращать инициализацию объекта:
+// если имя передаваемой переменной совпадает с названием свойства, то можно писать так:
+const age = 18;
+const movesTo = place => { console.log(`${user.name} движется к ${place}\n`) }
+
+user = { age, movesTo, name: 'Кларк' };
+
+console.log(user); // age: 18, name: 'Кларк' (имя свойства совпадает с именем переменной)
+user.movesTo('отчислению') // название метода совпадает с передаваемым методом в свойства объекта
+
+// ФУНКЦИЯ Object.fromEntries предоставляет возможность создания объекта из набора пар типа ключ-значение,
+// где ключ представляет название свойства.
+const personData = [ ['name', 'John' ], ['age', 18]]; // Массив содержит 2 подмассива, где в каждом из них первое значение - ключ, а второе - значение
+// const person = { 'name': 'John', 'age': 18 } будет аналогично тому, что представлено ниже
+const person = Object.fromEntries(personData); 
+
+// { name: 'John', age: 18 }
+console.log(person);
+// John 18
+console.log(person.name, person.age);
